93446cda69a87311875bc37f3dd4e78ffd5c979f
Duy Pham <duy.phamminh@enouvo.com>
2026-06-01 15:33:38 +0700

    chore: update AWS region default to ap-southeast-1

--- src/config/aws.config.ts
+++ src/config/aws.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('aws', () => ({
-  region: process.env.AWS_REGION ?? 'us-east-1',
+  region: process.env.AWS_REGION ?? 'ap-southeast-1',
   s3BucketName: process.env.S3_BUCKET_NAME ?? '',
   orderQueueUrl: process.env.ORDER_QUEUE_URL ?? '',
   orderTopicArn: process.env.ORDER_EVENTS_TOPIC_ARN ?? '',
